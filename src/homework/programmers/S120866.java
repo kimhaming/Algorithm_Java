22bf442e472a65e30febc0cb6541f2209fcf6e40
kimhaming <kimhamin83@gmail.com>
2023-12-22 17:16:12 +0900

    ✨ [level 0] 120866

---
+++ src/homework/programmers/S120866.java
@@ -0,0 +1,15 @@
+package homework.programmers;
+
+public class S120866 {
+    public static void main(String[] args) {
+        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
+        System.out.println(solution(board));
+        // 이차원 배열 (3, 2) 는 무엇을 뜻하는지
+        //  문제 이해가 안돼요 ㅠ ㅠ
+    }
+
+    public static int solution(int[][] board) {
+        int answer = 0;
+        return answer;
+    }
+}
