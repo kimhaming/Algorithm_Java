59d8a07359352e1d5b6d9ccd64d7e4ef991bea58
kimhaming <121594060+kimhaming@users.noreply.github.com>
2023-12-25 18:56:29 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 248 ms, Memory: 13560 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= 2 * i; j++) {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
