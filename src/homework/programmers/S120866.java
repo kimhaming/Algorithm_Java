8e786ccf9821f486402d19b745b7fc1b99f9f9ae
kimhaming <kimhamin83@gmail.com>
2023-12-27 16:50:48 +0900

    feat: S120866 - '안전지대'

--- src/homework/programmers/S120866.java
+++ src/homework/programmers/S120866.java
@@ -4,9 +4,13 @@ public class S120866 {
     public static void main(String[] args) {
         int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
         System.out.println(solution(board));
-        // 이차원 배열 (3, 2) 는 무엇을 뜻하는지
-        //  문제 이해가 안돼요 ㅠ ㅠ
     }
+    /**
+     * Todo:
+     *  1. (2,2) (2행 2열) 에 지뢰가 있다면 board 배열에 1이 찍힌다.
+     *  2. 인접된 행렬들은 위험한 지역으로 여긴다.
+     *  3. 총 칸 수 - 1 - 위험한 지역 수 = 안전한 지역 수
+     */
 
     public static int solution(int[][] board) {
         int answer = 0;
