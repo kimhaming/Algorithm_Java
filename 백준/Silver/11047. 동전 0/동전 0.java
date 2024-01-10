001ecad258d60ea346f1841fcdfe766fd6d987f9
kimhaming <121594060+kimhaming@users.noreply.github.com>
2024-01-10 17:47:06 +0900

    [Silver IV] Title: 동전 0, Time: 80 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int[] coins = new int[n];
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+           
+        }
+        
+        Arrays.sort(coins);
+        
+        for (int i = n - 1; i >= 0; i--) {
+            int currentCoin = k / coins[i];
+            
+            count += currentCoin;
+            k -= currentCoin * coins[i];
+            
+            if (k ==0) {
+                break;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
