a2b412a7747e84af36ff3a36f21684735bf0d832
kimhaming <kimhamin83@gmail.com>
2024-01-30 22:25:35 +0900

    feat: [240130] S2235. Add Two Integers

---
+++ src/leetcode/S2235.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class S2235 {
+    public static void main(String[] args) {
+        System.out.println(sum(-10, 4));
+        // Output: 17
+    }
+
+    // num1 + num2
+    public static int sum(int num1, int num2) {
+        int answer = 0;
+
+        answer = num1 + num2;
+
+        return answer;
+    }
+}
