e00d13bc16d9e6cb7ff1667ffb6056e1794cd158
kimhaming <121594060+kimhaming@users.noreply.github.com>
2024-03-27 00:34:56 +0900

    [level 0] Title: 문자열 섞기, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/README.md
+++ 프로그래머스/0/181942. 문자열 섞기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 섞기 - 181942 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181942) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181942?language=java) 
 
 ### 성능 요약
 
-메모리: 75.2 MB, 시간: 10.58 ms
+메모리: 75.6 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 26일 23:36:58
+2024년 03월 27일 00:34:53
 
 ### 문제 설명
 
--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,10 +1,9 @@
 class Solution {
-    public String solution(String str1, String str2) {
-        String answer = "";
+    public StringBuilder solution(String str1, String str2) {
+        StringBuilder answer = new StringBuilder();
 
         for (int i = 0; i < str1.length(); i++) {
-            answer += str1.charAt(i);
-            answer += str2.charAt(i);
+            answer.append(str1.charAt(i)).append(str2.charAt(i));
         }
         
         return answer;
