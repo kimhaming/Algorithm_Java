54fc787278f266b7e012b58da64078557ff14b4f
kimhaming <121594060+kimhaming@users.noreply.github.com>
2024-04-06 21:46:52 +0900

    [level 0] Title: 안전지대, Time: 0.08 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -2,34 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] board) {
-         ArrayList<int[]> list = new ArrayList<>();
-        int answer = 0;
-        int[] dx = {0,1,-1,0,-1,1,1,-1};
-        int[] dy = {1,0,0,-1,-1,1,-1,1};
-        
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == 1){
-                    int[] temp = {i,j};
-                    list.add(temp);
-                } 
+         int safe = 0;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 1) {
+                    for (int p = Math.max(0, i - 1); p <= Math.min(board.length - 1, i + 1); p++) {
+                        for (int q = Math.max(0, j - 1); q <= Math.min(board[i].length - 1, j + 1); q++) {
+                            if (!(p == i && q == j) && !(board[p][q] == 1)) {
+                                board[p][q] = 2;
+                            }
+                        }
+                    }
+                }
             }
         }
-        
-        for(int a = 0 ; a < list.size(); a++){
-            int i = list.get(a)[0];
-            int j = list.get(a)[1];
-            for(int k = 0 ; k < 8; k++){
-                        if(dx[k] + i >= 0 && dy[k] + j >= 0 && dx[k] + i <= board.length-1 && dy[k] + j <= board.length-1)
-                            board[dx[k] + i][dy[k] + j] = 1;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 0) {
+                    safe++;
+                }
             }
         }
-        
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == 0) answer+=1;
-            }
-        }
-        return answer;
+
+        return safe;
     }
 }
