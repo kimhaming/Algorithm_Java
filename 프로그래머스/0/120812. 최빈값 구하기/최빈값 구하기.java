e2c9776562f4d41416fb94807699dc02d973808c
kimhaming <121594060+kimhaming@users.noreply.github.com>
2024-04-14 00:38:56 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.39 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        Arrays.sort(array);
+
+        // 최빈값
+        int mode = 0;
+        // 최빈값의 개수를 저장할 변수
+        int maxCount = 0;
+
+        // 주어진 배열에서 가장 큰 값
+        int max = array[array.length - 1];
+
+        // array의 요소 값마다 얼마나 출현하는지 카운트할 배열 생성
+        int[] count = new int[max + 1];
+
+        // 각 숫자의 개수 count 배열에 기록
+        for (int i = 0; i < array.length; i++) {
+            count[array[i]]++;
+        }
+
+        // 최빈값 찾기
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > maxCount) {
+                mode = i;
+                maxCount = count[i];
+            }
+        }
+
+        // 최빈값 또 있는지 확인하기
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == maxCount && i != mode) {
+                return -1;
+            }
+        }
+
+        return mode;
+    }
+}
