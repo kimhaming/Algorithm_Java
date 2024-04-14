b18198987ccc0e27bb61e9df46c01e5a56461c4b
kimhaming <121594060+kimhaming@users.noreply.github.com>
2024-04-14 10:49:29 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[][] dots) {
         int answer = 0;
-        
+
         int x1 = dots[0][0];
         int y1 = dots[0][1];
         int x2 = dots[1][0];
@@ -11,18 +11,24 @@ class Solution {
         int x4 = dots[3][0];
         int y4 = dots[3][1];
 
-        // 2개의 기울기가 같은지 확인하기 위해 기울기 변수 2개 만들기
-        double slope1 = (double) (y1 - y2) / (x1 - x2);
-        double slope2 = (double) (y3 - y4) / (x3 - x4);
-        if (slope1 == slope2) answer = 1;
+        // 기울기 변수 2개
+        double slope1 = (double) (y1 - y3) / (x1 - x3);
+        double slope2 = (double) (y2 - y4) / (x2 - x4);
+        if (slope1 == slope2) {
+            answer = 1;
+        }
 
-        slope1 = (double) (y3 - y1) / (x3 - x1);
-        slope2 = (double) (y4 -y2) / (x4 - x2);
-        if (slope1 == slope2) answer = 1;
-
-        slope1 = (double) (y4 -y1) / (x4 - x1);
+        slope1 = (double) (y1 - y4) / (x1 - x4);
         slope2 = (double) (y3 - y2) / (x3 - x2);
-        if (slope1 == slope2) answer = 1;
+        if (slope1 == slope2) {
+            answer = 1;
+        }
+
+        slope1 = (double) (y1 - y2) / (x1 - x2);
+        slope2 = (double) (y3 - y4) / (x3 - x4);
+        if (slope1 == slope2) {
+            answer = 1;
+        }
 
         return answer;
     }
