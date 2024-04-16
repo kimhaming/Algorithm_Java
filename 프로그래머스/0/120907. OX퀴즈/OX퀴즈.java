a2b63bff2f7420a94d0a89b58033dda850fa7c3f
kimhaming <121594060+kimhaming@users.noreply.github.com>
2024-04-16 10:07:21 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -1,30 +1,25 @@
 class Solution {
     public String[] solution(String[] quiz) {
         String[] answer = new String[quiz.length];
-
+        // quiz 요소 순회하면서 문자열을 뜯어서 실제 연산 확인하기
+        // 첫번째 순회할 때 숫자와 부호 독립적으로 뽑아내서 정답인지 체크하고 결과를 result에 저장하는 것까지 해야된다
         for (int i = 0; i < quiz.length; i++) {
+            String[] str = quiz[i].split(" ");
+            int firstNum = Integer.parseInt(str[0]);
+            int secondNum = Integer.parseInt(str[2]);
+            int thirdNum = Integer.parseInt(str[4]);
+            String operator = str[1];
 
-            String str = quiz[i];
-            String[] arr = str.split(" ");
-            int x = Integer.parseInt(arr[0]);
-            int y = Integer.parseInt(arr[2]);
-            int z = Integer.parseInt(arr[4]);
-            String operator = arr[1];
+            boolean isCorrect = operator.equals("+") ?
+                    firstNum + secondNum == thirdNum :
+                    firstNum - secondNum == thirdNum;
 
-            int result = 0;
-            if (operator.equals("+")) {
-                result = x + y;
-            }
-            else {
-                result = x - y;
-            }
-            if (result == z) {
+            if (isCorrect) {
                 answer[i] = "O";
             }
-            else {
-                answer[i] = "X";
+            else answer[i] = "X";
             }
-        }
+
         return answer;
     }
 }
