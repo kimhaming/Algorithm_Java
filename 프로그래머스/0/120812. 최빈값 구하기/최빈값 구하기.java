a8616ba21152ca3dd3b972783d5ed047b1cdc970
kimhaming <121594060+kimhaming@users.noreply.github.com>
2024-04-17 20:38:43 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.06 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,30 +1,39 @@
 import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] array) {
-        if (array.length == 1) {
-            return array[0];
+
+        // 최빈값
+        int mode = 0;
+        // 최빈값의 개수를 저장할 변수
+        int maxCount = 0;
+
+        // 주어진 배열에서 가장 큰 값
+        int max = array[array.length - 1];
+
+        // array의 요소 값마다 얼마나 출현하는지 카운트할 배열 생성
+        int[] count = new int[1000];
+
+        // 각 숫자의 개수 count 배열에 기록
+        for (int i = 0; i < array.length; i++) {
+            count[array[i]]++;
         }
 
-        Map<Integer, Long> map = Arrays.stream(array)
-                .boxed()    // int를 Map의 key 타입으로 boxing
-                // {0=1, 1=1, 2=2, 4=1, 5=1, 10=3} 으로 map에 저장된다
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
         // 최빈값 찾기
-        // Map.Entry: Map의 키와 값 쌍을 하나의 객체로 얻을 수 있다
-        Optional<Map.Entry<Integer, Long>> mostFrequency = map.entrySet().stream()
-                .max(Map.Entry.comparingByValue()); // 객체에는 3이 담긴다.
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > maxCount) {
+                mode = i;
+                maxCount = count[i];
+            }
+        }
 
-        long maxFrequency = mostFrequency.map(Map.Entry::getValue).orElse(-1L);
+        // 최빈값 또 있는지 확인하기
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == maxCount && i != mode) {
+                return -1;
+            }
+        }
 
-        // 여기서 for문처럼 map의 값을 모두 스트림으로 변환해서 val 인자에 담아서 maxFrequency와 같은 val에 해당하는 값을 찾고, 찾은 수의 개수를 세어 count에 담는다
-        long count = map.values().stream().filter(val -> val == maxFrequency).count();
-
-        return count == 1 ? mostFrequency.get().getKey() : -1;
+        return mode;
     }
 }
