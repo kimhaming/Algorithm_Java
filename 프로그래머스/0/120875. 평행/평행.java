a42102e48d969badd2c5c5665b4a9888136b67c3
kimhaming <121594060+kimhaming@users.noreply.github.com>
2024-04-17 20:49:43 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -2,34 +2,24 @@ class Solution {
     public int solution(int[][] dots) {
         int answer = 0;
 
-        int x1 = dots[0][0];
-        int y1 = dots[0][1];
-        int x2 = dots[1][0];
-        int y2 = dots[1][1];
-        int x3 = dots[2][0];
-        int y3 = dots[2][1];
-        int x4 = dots[3][0];
-        int y4 = dots[3][1];
+        if (slopeCalculator(dots[0], dots[1]) == slopeCalculator(dots[2], dots[3]))
+            return 1;
 
-        // 기울기 변수 2개
-        double slope1 = (double) (y1 - y3) / (x1 - x3);
-        double slope2 = (double) (y2 - y4) / (x2 - x4);
-        if (slope1 == slope2) {
-            answer = 1;
-        }
+        if (slopeCalculator(dots[0], dots[2]) == slopeCalculator(dots[1], dots[3]))
+            return 1;
 
-        slope1 = (double) (y1 - y4) / (x1 - x4);
-        slope2 = (double) (y3 - y2) / (x3 - x2);
-        if (slope1 == slope2) {
-            answer = 1;
-        }
-
-        slope1 = (double) (y1 - y2) / (x1 - x2);
-        slope2 = (double) (y3 - y4) / (x3 - x4);
-        if (slope1 == slope2) {
-            answer = 1;
-        }
+        if (slopeCalculator(dots[0], dots[3]) == slopeCalculator(dots[1], dots[2]))
+            return 1;
 
         return answer;
+    }
+
+    // 기울기 구하기 메소드 만들기
+    public static double slopeCalculator(int[] dot1, int[] dot2) {
+        double slope = 0;
+
+        slope = (double) (dot1[1] - dot2[1]) / (dot1[0] - dot2[0]);
+
+        return slope;
     }
 }
