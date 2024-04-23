68b4e37e3539e2788fb52b81b96b7be9c241baec
kimhaming <121594060+kimhaming@users.noreply.github.com>
2024-04-23 11:57:43 +0900

    [level 1] Title: 실패율, Time: 0.72 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,37 +1,41 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
+       // 각 스테이지의 실패율을 반환할 배열 생성
+        double[] answer = new double[N];
 
-        // key = 스테이지 번호, value = 실패율
-        Map<Integer, Double> map = new HashMap<>();
+        // stages 배열의 요소를 키로 저장, 키에 해당하는 숫자가 등장하는 횟수를 값으로 매핑
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 1; i <= N; i++) {
-            double failNum = 0;
-            double challengeNum = 0;
+        for (int i = 0; i < stages.length; i++) {
+            int value = 0;
 
-            for (int j = 0; j < stages.length; j++) {
-                if (stages[j] == i) {
-                    failNum++;
-                }
-                if (stages[j] >= i) {
-                    challengeNum++;
-                }
+            if (map.containsKey(stages[i])) {
+                value += 1;
+                map.put(stages[i], value);
+            } else {
+                map.put(stages[i], 0);
             }
-            double failRate = (challengeNum != 0) ? failNum / challengeNum : 0;
-            map.put(i, failRate);
+        }
+        // map에는 스테이지 현재 단계를 키, 해당 단계에서 실패한 사람 수를 값으로 매핑되어있다
+        // 실패율은 실패한 사람 수 / 도전한 사람 수
+        for (int i = 0; i < answer.length; i++) {
+            int challengers = N;
+            answer[i] = map.get(i) / challengers;
+            challengers -= map.get(i);
         }
 
-        List<Integer> keySet = new ArrayList<>(map.keySet());
-        keySet.sort((o1,o2)-> map.get(o2).compareTo(map.get(o1)));//value값으로 내림차순 정렬
+        Integer[] index = new Integer[answer.length];
 
-        for (int i=0;i<N;i++) {
-            answer[i] = keySet.get(i); //키값 즉 스테이지 번호를 담아줌
+        for (int i = 0; i < answer.length; i++) {
+            index[i] = i + 1;
         }
-        return answer;
-    }
+
+        Arrays.sort(index, Comparator.comparingDouble(i -> answer[i]).reversed());
+
+        return Arrays.stream(index).mapToInt(Integer::intValue).toArray();
 }
