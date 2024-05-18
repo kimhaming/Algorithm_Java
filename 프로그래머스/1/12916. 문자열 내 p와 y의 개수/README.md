4585656535e849168564e4265cb856470f05c3e4
HaMin Kim <kimhamin83@gmail.com>
2024-05-18 13:17:06 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 1.47 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/README.md
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 p와 y의 개수 - 12916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12916) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12916?language=java) 
 
 ### 성능 요약
 
-메모리: 74.4 MB, 시간: 0.02 ms
+메모리: 75.3 MB, 시간: 1.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 18일 13:09:31
+2024년 05월 18일 13:17:02
 
 ### 문제 설명
 
--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -1,25 +1,9 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        int pNum = 0;
-        int yNum = 0;
+        s = s.toUpperCase();
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (c == 'p' || c == 'P') {
-                pNum++;
-            } else if (c == 'y' || c == 'Y') {
-                yNum++;
-            }
-        }
-
-        if (pNum == yNum) {
-            answer = true;
-        } else {
-            answer = false;
-        }
-
+        boolean answer = s.chars().filter(e -> 'P' == e).count() == s.chars().filter(e -> 'Y' == e).count();
+        
         return answer;
     }
 }
